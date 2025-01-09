41925b1fd2223ba78a2ebdac68cfc90a6788099e
Gaveyn <crstalhopkin@gmail.com>
2025-01-09 19:13:27 +0500

    Update users and files models

--- db/models/files.py
+++ db/models/files.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import Relationship
 
 from ..base_model import Model
@@ -13,5 +13,7 @@ class File(Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     path = Column(Integer)
     created_at = Column(DateTime, default=datetime.datetime.now)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     message = Relationship('Message', back_populates='file', cascade='all, delete-orphan')
+    user = Relationship('User', back_populates='files')
--- db/models/users.py
+++ db/models/users.py
@@ -19,4 +19,5 @@ class User(Model):
     code_expiration = Column(DateTime, nullable=True)
     created_at = Column(DateTime, default=datetime.datetime.now)
 
+    files = Relationship('File', back_populates='user', cascade='all, delete-orphan')
     chats = Relationship('Chat', back_populates='user', cascade='all, delete-orphan')
