6647b91936a610224a6b7d61326c622aac4bc526
Shkvaldev <shkvalgrozny@gmail.com>
2025-03-09 01:18:45 +0500

    initial integration rabbitmq in websockets

--- api/services/web_sockets.py
+++ api/services/web_sockets.py
@@ -1,14 +1,16 @@
 from fastapi import WebSocket
+from loguru import logger
 from pydantic import BaseModel
-from typing import Optional, Literal
+from typing import Optional, Literal, List
 from api.utils import get_current_user
 from db.models import User
 from api.services.messages import MessageService
 from api.connection_manager_ws import ConnectionManager
 
-from rabbitmq import import push_task
+from rabbitmq import push_task
 from rabbitmq.task import Task
 from mongodb.models import TaskStatus
+from mongodb.services import MongoBaseService
 
 class SendMessageSchema(BaseModel):
     action: Literal["send_message"]
@@ -36,6 +38,7 @@ class GetChatMessagesSchema(BaseModel):
 
 async def create_message(manager:ConnectionManager, data:dict, user:User, chat_id:int) -> None: 
     schema = SendMessageSchema(**data)
+    
     message = await MessageService().create(
         chat_id=schema.chat_id,
         content=schema.content,
@@ -52,6 +55,16 @@ async def create_message(manager:ConnectionManager, data:dict, user:User, chat_i
         "file_id": message.file_id,
         "timestamp": str(message.timestamp)
     }
+    
+    # Creating task for rabbitmq
+    # TODO!: add here parsing message or other stuff to define `type` header for worker
+    if message_data["role"] != "bot":
+        task = Task(
+            status="pending",
+            data=message_data
+        )
+        await push_task(task=task, headers={"type": "llm"})
+    # Sending message to WS
     await manager.send_message(chat_id, message_data)
 
 async def delete_message(manager:ConnectionManager, data:dict, user:User, chat_id:int) -> None:
@@ -84,6 +97,22 @@ async def get_message( manager:ConnectionManager, data:dict, user:User, chat_id:
 
 async def get_chat_messages( manager:ConnectionManager, data:dict, user:User, chat_id:int) -> None:
     schema = GetChatMessagesSchema(**data)
+    
+    # Checking rabbitmq tasks
+    task_statuses: Optional[List[TaskStatus]] = await MongoBaseService.find_all(TaskStatus, filters={"status": "done"}) 
+    if task_statuses:
+        for task_status in task_statuses:
+            try:
+                await MessageService().create(
+                    chat_id=task_status.data["chat_id"],
+                    content=task_status.data["content"],
+                    role="bot",
+                    user_id=task_status.data["user_id"],
+                    file_id=task_status.data["file_id"] or None
+                )
+            except Exception as e:
+                logger.error(f"Failed to create new message from bot: {e}")
+
     messages = await MessageService().get_messages(chat_id=schema.chat_id, user_id=user.id)
     messages_data = [
         {
